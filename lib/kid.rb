b4825f3b122ca57b2cfdb6ea503cedab1ec86f3b
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-10-16 03:36:31 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,6 +2,7 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid
+  extend MetaDancing
   include Dance
 
   attr_accessor :name
@@ -9,5 +10,6 @@ class Kid
   def initialize(name)
     @name = name
   end
+
 
 end
