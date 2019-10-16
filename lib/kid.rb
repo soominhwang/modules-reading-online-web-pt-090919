09aa47398bbf71338077430cb66963a9defd7829
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-10-16 03:41:00 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,8 +2,11 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+
   extend MetaDancing
-  
+
   include Dance
 
   attr_accessor :name
